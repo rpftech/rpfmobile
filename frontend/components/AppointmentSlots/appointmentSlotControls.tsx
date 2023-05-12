5de8cf76d27459e4f1eb977781bf91ec226e0d0c
Mayowa Adeniyi <mayowaadeniyi@hotmail.com>
2023-05-12 17:13:32 +0100

    feat: Add button animation to indicate when button has been pressed

--- frontend/components/AppointmentSlots/appointmentSlotControls.tsx
+++ frontend/components/AppointmentSlots/appointmentSlotControls.tsx
@@ -1,25 +1,87 @@
-import {StyleSheet, View} from "react-native";
+import {Animated, Pressable, StyleSheet, View} from "react-native";
 import {AntDesign} from "@expo/vector-icons";
 import {Button, Text} from "react-native-paper";
-import React from "react";
+import React, {useRef, useState} from "react";
 
 interface Props {
     formattedStartDate: (format: string) => string;
     onButtonDateChange: (direction: string) => void;
     handleCalendarDisplay: (action: string) => void;
+};
+
+interface ButtonProps {
+    scale: Animated.Value;
+    opacity: Animated.Value;
 }
 
 const AppointmentSlotControls = ({ formattedStartDate, onButtonDateChange, handleCalendarDisplay }: Props) => {
+    const leftButtonProps = {
+        scale: useRef(new Animated.Value(1)).current,
+        opacity: useRef(new Animated.Value(1)).current
+    };
+    const rightButtonProps = {
+        scale: useRef(new Animated.Value(1)).current,
+        opacity: useRef(new Animated.Value(1)).current
+    };
+    const buttonProps = {
+        left: leftButtonProps,
+        right: rightButtonProps
+    };
+    const [disableButtons, setDisableButtons] = useState(false);
+
+    const scaleButton = (buttonProps: ButtonProps) => {
+        return Animated.sequence([
+            Animated.timing(buttonProps.scale, {
+                toValue: 1.4,
+                duration: 250,
+                useNativeDriver: true
+            }),
+            Animated.timing(buttonProps.scale, {
+                toValue: 1,
+                duration: 250,
+                useNativeDriver: true
+            })
+        ])
+    };
+    const handlePressOut = (direction: string) => {
+        setDisableButtons(true);
+        const buttonProp = buttonProps[direction as keyof typeof buttonProps];
+        scaleButton(buttonProp).start(({ finished }) => {
+            onButtonDateChange(direction);
+            setDisableButtons(false);
+        })
+    };
+
+    const setScale = (scale: Animated.Value | number) => {
+        return {
+            transform: [
+                {scale: scale}
+            ]
+        };
+    };
+
+    const handlePressIn = () => {
+        console.log('Pressing in!')
+    };
+
     return (
         <View style={styles.header}>
-            <AntDesign onPress={() => onButtonDateChange('left')} name="leftcircle" size={24} color="black" />
+            <Animated.View style={[styles.animatedView, setScale(leftButtonProps.scale)]}>
+                <Pressable style={{ opacity: disableButtons ? 0.5 : 1 }} disabled={disableButtons} onPressIn={handlePressIn} onPressOut={() => handlePressOut('left')}>
+                    <AntDesign name="leftcircle" size={24} color="black" />
+                </Pressable>
+            </Animated.View>
             <View style={styles.headerTextContainer}>
                 <Text>{formattedStartDate('dddd Do MMMM YYYY')}</Text>
                 <Button compact={true} mode='elevated' onPress={() => handleCalendarDisplay('reset')}>Change date</Button>
             </View>
             {/* Major functionality to work on next: */}
             {/* 2. Figure out how to add some kind of animation to show the button has been clicked */}
-            <AntDesign onPress={() => onButtonDateChange('right')} name="rightcircle" size={24} color="black" />
+            <Animated.View style={[styles.animatedView, setScale(rightButtonProps.scale)]}>
+                <Pressable style={{ opacity: disableButtons ? 0.5 : 1 }} disabled={disableButtons} onPressIn={handlePressIn} onPressOut={() => handlePressOut('right')}>
+                    <AntDesign disabled={disableButtons} name="rightcircle" size={24} color="black" />
+                </Pressable>
+            </Animated.View>
         </View>
     )
 };
@@ -49,8 +111,15 @@ const styles = StyleSheet.create({
     headerTextContainer: {
         alignItems: "center"
     },
+    animatedView: {
+        transform: [
+            {perspective: 1000},
+        ]
+    },
     dateButton: {
-
+        transform: [
+            {scale: 1.5},
+        ]
     },
     view: {
         flex: 1
